cf24fc23de30381ad25d27d161584cc4ef055927
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-14 18:58:07 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -1,1 +1,73 @@
+function unhide(divID) {
+  var item = document.getElementById(divID);
+  if (item) {
+    item.className='unhidden';
+  }
+  else {
+    document.title = "other";
+  }
+}
 
+function hide(divID) {
+  var item = document.getElementById(divID);
+  if (item) {
+    item.className='hidden';
+  }
+}
+
+function showHide(what) {
+  if (what == "top") {
+    showPageElement("nation");
+    showPageElement("region");
+    showPageElement("state-level");
+    return false;
+  }
+  if (what == "nation") {
+    showPageElement("nation");
+    hidePageElement("region");
+    hidePageElement("state-level")
+    return false;
+  }
+  if (what == "region") {
+    hidePageElement("nation");
+    showPageElement("region");
+    hidePageElement("state-level");
+    return false;
+  }
+  if (what == "state-level") {
+    hidePageElement("nation");
+    hidePageElement("region");
+    showPageElement("state-level");
+    return false;
+  }
+  return false;
+}
+function showPageElement(what)  
+{  
+    var obj = typeof what == 'object'  
+        ? what : document.getElementById(what);  
+  
+    obj.style.display = 'block';  
+    return false;  
+}  
+  
+function hidePageElement(what)  
+{  
+    var obj = typeof what == 'object'  
+        ? what : document.getElementById(what);  
+  
+    obj.style.display = 'none';  
+    return false;  
+}  
+  
+function togglePageElementVisibility(what)  
+{  
+    var obj = typeof what == 'object'  
+        ? what : document.getElementById(what);  
+  
+    if (obj.style.display == 'none')  
+        obj.style.display = 'block';  
+    else  
+        obj.style.display = 'none';  
+    return false;  
+}  
