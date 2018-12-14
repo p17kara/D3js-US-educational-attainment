7c9ee19ba16e9369d053287885cedfd32a47aff8
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-14 19:45:22 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -1,73 +1,52 @@
-function unhide(divID) {
-  var item = document.getElementById(divID);
-  if (item) {
-    item.className='unhidden';
-  }
-  else {
-    document.title = "other";
-  }
-}
+const dataset = [36.5,41.4, 99.9 , 57.3];
+const datasetTitles = ["Belgium", "United States"];
 
-function hide(divID) {
-  var item = document.getElementById(divID);
-  if (item) {
-    item.className='hidden';
-  }
-}
 
-function showHide(what) {
-  if (what == "top") {
-    showPageElement("nation");
-    showPageElement("region");
-    showPageElement("state-level");
-    return false;
-  }
-  if (what == "nation") {
-    showPageElement("nation");
-    hidePageElement("region");
-    hidePageElement("state-level")
-    return false;
-  }
-  if (what == "region") {
-    hidePageElement("nation");
-    showPageElement("region");
-    hidePageElement("state-level");
-    return false;
-  }
-  if (what == "state-level") {
-    hidePageElement("nation");
-    hidePageElement("region");
-    showPageElement("state-level");
-    return false;
-  }
-  return false;
-}
-function showPageElement(what)  
-{  
-    var obj = typeof what == 'object'  
-        ? what : document.getElementById(what);  
-  
-    obj.style.display = 'block';  
-    return false;  
-}  
-  
-function hidePageElement(what)  
-{  
-    var obj = typeof what == 'object'  
-        ? what : document.getElementById(what);  
-  
-    obj.style.display = 'none';  
-    return false;  
-}  
-  
-function togglePageElementVisibility(what)  
-{  
-    var obj = typeof what == 'object'  
-        ? what : document.getElementById(what);  
-  
-    if (obj.style.display == 'none')  
-        obj.style.display = 'block';  
-    else  
-        obj.style.display = 'none';  
-    return false;  
-}  
+d3.select(".container")
+    .append("h2")
+    .text("Minimum wages");
+
+
+const w = d3.max(dataset)*2;
+const h = dataset.length*40;
+
+const svg = d3.select(".container")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", (d, i) => i*40)
+    .attr("height", (d, i) => 35)
+    .attr("class", "bar")
+    .append("title")
+    .text((d, i) => datasetTitles[i]);
+
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text((d) => d)
+    .attr("y", (d, i) => i * 40 + 25)
+    .attr("class", "data-point");
+
+
+
+svg.selectAll("rect")
+    .transition()
+    .duration(1000)
+    .delay((d, i) => 1000 + i*200)
+    .attr("width", (d, i) => d*1.5);
+
+
+svg.selectAll("text")
+    .transition()
+    .duration(1000)
+    .delay((d, i) => 1000 + i*200)
+    .style("opacity", 1)
+    .attr("x", (d, i) => d*1.5 + 5);
