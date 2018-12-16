379afae0574ce5219b66679335eb3fe974ab42d2
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-16 14:54:41 +0200

    Create script_6.js

---
+++ assets/scripts/script_6.js
@@ -0,0 +1,52 @@
+const dataset = [117  ,49  , 89  , 128 , 124, 79, 181 , 67, 92, 104 ];
+const datasetTitles = ["Belgium", "Bulgaria", "Czechia", "Denmark", "Germany", "Estonia", "Ireland", "Greece", "Spain", "France"];
+
+
+d3.select(".container")
+    .append("h3")
+    .text("GDP per capita in PPS");
+
+
+const w = d3.max(dataset)*2;
+const h = dataset.length*40;
+
+const svg = d3.select(".container")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", (d, i) => i*40)
+    .attr("height", (d, i) => 35)
+    .attr("class", "bar")
+    .append("title")
+    .text((d, i) => datasetTitles[i]);
+
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text((d) => d)
+    .attr("y", (d, i) => i * 40 + 25)
+    .attr("class", "data-point");
+
+
+
+svg.selectAll("rect")
+    .transition()
+    .duration(1000)
+    .delay((d, i) => 1000 + i*200)
+    .attr("width", (d, i) => d*1.5);
+
+
+svg.selectAll("text")
+    .transition()
+    .duration(1000)
+    .delay((d, i) => 1000 + i*200)
+    .style("opacity", 1)
+    .attr("x", (d, i) => d*1.5 + 5);
