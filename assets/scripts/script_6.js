e7563ea6251385b55652454a331cf95e36db4549
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-17 13:19:13 +0200

    Update script_6.js

--- assets/scripts/script_6.js
+++ assets/scripts/script_6.js
@@ -1,58 +1,4 @@
-var pie=new d3pie("pieChart",{ 
-  size:{
-    canvasHeight:400,
-    canvasWidth: 500
-  },
-  data:{
-    content:[
-      {
-        label:"Belgium",
-        value:1701596,
-        color:"#FFA500"
-      },
-      {
-        label:"Bulgaria",
-        value:3446512,
-        color:"#FF8C00"
-      },
-      {
-        label:"Czechia",
-        value:3913450,
-        color:"#FF7F50"
-      },
-      {
-        label:"Denmark",
-        value:2849335 ,
-        color:"#FF6347"
-      },
-      {
-        label:"Germany",
-        value:2193687, 
-        color:"#FF4500"
-      },
-	  {
-        label:"Estonia",
-        value:2012754, 
-        color:"#FFD700"
-      },
-      {
-        label:"Greece",
-        value:2193687, 
-        color:"#FF4500"
-      },   
-      {
-        label:"Spain",
-        value:2193687, 
-        color:"#FF4500"
-      }, 
-      {
-        label:"France",
-        value:2193687, 
-        color:"#FF4500"
-      },     
-    ]
-  },
-  chart = {
+chart = {
   const svg = d3.select(DOM.svg(width, height));
 
   svg.append("g")
@@ -72,3 +18,55 @@ var pie=new d3pie("pieChart",{
   
   return svg.node();
 }
+
+height = 500
+margin = ({top: 20, right: 30, bottom: 30, left: 40})
+
+x = d3.scaleTime()
+    .domain(d3.extent(data, d => d.date))
+    .range([margin.left, width - margin.right])
+
+y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.value)]).nice()
+    .range([height - margin.bottom, margin.top])
+
+xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+
+yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.select(".tick:last-of-type text").clone()
+        .attr("x", 3)
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text(data.y))
+
+line = d3.line()
+    .defined(d => !isNaN(d.value))
+    .x(d => x(d.date))
+    .y(d => y(d.value))
+data = Array(1280) [
+0: Object {date: 2007-04-23, value: 93.24}
+  1: Object {date: 2007-04-24, value: 95.35}
+  2: Object {date: 2007-04-25, value: 98.84}
+  3: Object {date: 2007-04-26, value: 99.92}
+  4: Object {date: 2007-04-29, value: 99.8}
+  5: Object {date: 2007-05-01, value: 99.47}
+  6: Object {date: 2007-05-02, value: 100.39}
+  7: Object {date: 2007-05-03, value: 100.4}
+  8: Object {date: 2007-05-04, value: 100.81}
+  9: Object {date: 2007-05-07, value: 103.92}
+  10: Object {date: 2007-05-08, value: 105.06}
+  11: Object {date: 2007-05-09, value: 106.88}
+  12: Object {date: 2007-05-09, value: 107.34}
+  13: Object {date: 2007-05-10, value: 108.74}
+  14: Object {date: 2007-05-13, value: 109.36}
+  15: Object {date: 2007-05-14, value: 107.52}
+  16: Object {date: 2007-05-15, value: 107.34}
+  17: Object {date: 2007-05-16, value: 109.44}
+  18: Object {date: 2007-05-17, value: 110.02}
+  19: Object {date: 2007-05-20, value: 111.98}
+	]
