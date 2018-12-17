d7a22ae516a75469c92263adb7f595e89507463c
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-17 13:13:30 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -37,3 +37,91 @@ chart = {
 
   return svg.node();
 }
+
+data = Array(18) [
+  0: Object {
+  name: "<5"
+  value: 19912018
+}
+  1: Object {
+  name: "5-9"
+  value: 20501982
+}
+  2: Object {
+  name: "10-14"
+  value: 20679786
+}
+  3: Object {
+  name: "15-19"
+  value: 21354481
+}
+  4: Object {
+  name: "20-24"
+  value: 22604232
+}
+  5: Object {
+  name: "25-29"
+  value: 21698010
+}
+  6: Object {
+  name: "30-34"
+  value: 21183639
+}
+  7: Object {
+  name: "35-39"
+  value: 19855782
+}
+  8: Object {
+  name: "40-44"
+  value: 20796128
+}
+  9: Object {
+  name: "45-49"
+  value: 21370368
+}
+  10: Object {
+  name: "50-54"
+  value: 22525490
+}
+  11: Object {
+  name: "55-59"
+  value: 21001947
+}
+  12: Object {
+  name: "60-64"
+  value: 18415681
+}
+  13: Object {
+  name: "65-69"
+  value: 14547446
+}
+  14: Object {
+  name: "70-74"
+  value: 10587721
+}
+  15: Object {
+  name: "75-79"
+  value: 7730129
+}
+  16: Object {
+  name: "80-84"
+  value: 5811429
+}
+  17: Object {
+  name: "≥85"
+  value: 5938752
+}
+]
+color = d3.scaleOrdinal()
+    .domain(data.map(d => d.name))
+    .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length).reverse())
+height = Math.min(width, 500)
+arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(Math.min(width, height) / 2 - 1)
+arcLabel = {
+  const radius = Math.min(width, height) / 2 * 0.8;
+  return d3.arc().innerRadius(radius).outerRadius(radius);
+pie = d3.pie()
+    .sort(null)
+    .value(d => d.value)
