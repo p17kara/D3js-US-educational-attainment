c22485c01437f750f957ce3149f2374bf90b3b13
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-17 13:16:12 +0200

    Update script_5.js

--- assets/scripts/script_5.js
+++ assets/scripts/script_5.js
@@ -50,3 +50,51 @@ chart = {
   
   return svg.node();
 }
+data = Array(26) [
+  0: Object {name: "E", value: 0.12702}
+  1: Object {name: "T", value: 0.09056}
+  2: Object {name: "A", value: 0.08167}
+  3: Object {name: "O", value: 0.07507}
+  4: Object {name: "I", value: 0.06966}
+  5: Object {name: "N", value: 0.06749}
+  6: Object {name: "S", value: 0.06327}
+  7: Object {name: "H", value: 0.06094}
+  8: Object {name: "R", value: 0.05987}
+  9: Object {name: "D", value: 0.04253}
+  10: Object {name: "L", value: 0.04025}
+  11: Object {name: "C", value: 0.02782}
+  12: Object {name: "U", value: 0.02758}
+  13: Object {name: "M", value: 0.02406}
+  14: Object {name: "W", value: 0.0236}
+  15: Object {name: "F", value: 0.02288}
+  16: Object {name: "G", value: 0.02015}
+  17: Object {name: "Y", value: 0.01974}
+  18: Object {name: "P", value: 0.01929}
+  19: Object {name: "B", value: 0.01492}
+  
+  data = (await require("@observablehq/alphabet"))
+  .slice()
+  .sort((a, b) => b.frequency - a.frequency)
+  .map(({letter, frequency}) => ({name: letter, value: frequency}))
+  
+  x = d3.scaleBand()
+    .domain(data.map(d => d.name))
+    .range([margin.left, width - margin.right])
+    .padding(0.1)
+
+y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.value)]).nice()
+    .range([height - margin.bottom, margin.top])
+
+xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x)
+        .tickSizeOuter(0))
+
+yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select(".domain").remove())
+height = 500
+
+margin = ({top: 20, right: 0, bottom: 30, left: 40})
