784f34c8439f1bf1598f42707c5ee4b84212f0a9
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-17 13:21:47 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -1,52 +1,127 @@
-const dataset = [11,351,727 ,7,101,859 , 10,578,820  , 5,748,769, 82,521,653, 1,315,635, 4,784,383, 10,768,193, 46,527,039, 66,989,083 ];
-const datasetTitles = ["Belgium", "Bulgaria", "Czechia", "Denmark", "Germany", "Estonia", "Ireland", "Greece", "Spain", "France"];
+chart = {
+  const arcs = pie(data);
 
+  const svg = d3.select(DOM.svg(width, height))
+      .attr("text-anchor", "middle")
+      .style("font", "12px sans-serif");
 
-d3.select(".container")
-    .append("h1")
-    .text("Population");
-
-
-const w = d3.max(dataset)*2;
-const h = dataset.length*40;
-
-const svg = d3.select(".container")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h);
-
-
-svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("x", 0)
-    .attr("y", (d, i) => i*40)
-    .attr("height", (d, i) => 35)
-    .attr("class", "bar")
+  const g = svg.append("g")
+      .attr("transform", `translate(${width / 2},${height / 2})`);
+  
+  g.selectAll("path")
+    .data(arcs)
+    .enter().append("path")
+      .attr("fill", d => color(d.data.name))
+      .attr("stroke", "white")
+      .attr("d", arc)
     .append("title")
-    .text((d, i) => datasetTitles[i]);
+      .text(d => `${d.data.name}: ${d.data.value.toLocaleString()}`);
 
-svg.selectAll("text")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .text((d) => d)
-    .attr("y", (d, i) => i * 40 + 25)
-    .attr("class", "data-point");
+  const text = g.selectAll("text")
+    .data(arcs)
+    .enter().append("text")
+      .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+      .attr("dy", "0.35em");
+  
+  text.append("tspan")
+      .attr("x", 0)
+      .attr("y", "-0.7em")
+      .style("font-weight", "bold")
+      .text(d => d.data.name);
+  
+  text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
+      .attr("x", 0)
+      .attr("y", "0.7em")
+      .attr("fill-opacity", 0.7)
+      .text(d => d.data.value.toLocaleString());
 
+  return svg.node();
+}
 
-
-svg.selectAll("rect")
-    .transition()
-    .duration(1000)
-    .delay((d, i) => 1000 + i*200)
-    .attr("width", (d, i) => d*1.5);
-
-
-svg.selectAll("text")
-    .transition()
-    .duration(1000)
-    .delay((d, i) => 1000 + i*200)
-    .style("opacity", 1)
-    .attr("x", (d, i) => d*1.5 + 5);
+data = Array(18) [
+  0: Object {
+  name: "<5"
+  value: 19912018
+}
+  1: Object {
+  name: "5-9"
+  value: 20501982
+}
+  2: Object {
+  name: "10-14"
+  value: 20679786
+}
+  3: Object {
+  name: "15-19"
+  value: 21354481
+}
+  4: Object {
+  name: "20-24"
+  value: 22604232
+}
+  5: Object {
+  name: "25-29"
+  value: 21698010
+}
+  6: Object {
+  name: "30-34"
+  value: 21183639
+}
+  7: Object {
+  name: "35-39"
+  value: 19855782
+}
+  8: Object {
+  name: "40-44"
+  value: 20796128
+}
+  9: Object {
+  name: "45-49"
+  value: 21370368
+}
+  10: Object {
+  name: "50-54"
+  value: 22525490
+}
+  11: Object {
+  name: "55-59"
+  value: 21001947
+}
+  12: Object {
+  name: "60-64"
+  value: 18415681
+}
+  13: Object {
+  name: "65-69"
+  value: 14547446
+}
+  14: Object {
+  name: "70-74"
+  value: 10587721
+}
+  15: Object {
+  name: "75-79"
+  value: 7730129
+}
+  16: Object {
+  name: "80-84"
+  value: 5811429
+}
+  17: Object {
+  name: "≥85"
+  value: 5938752
+}
+]
+color = d3.scaleOrdinal()
+    .domain(data.map(d => d.name))
+    .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length).reverse())
+height = Math.min(width, 500)
+arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(Math.min(width, height) / 2 - 1)
+arcLabel = {
+  const radius = Math.min(width, height) / 2 * 0.8;
+  return d3.arc().innerRadius(radius).outerRadius(radius);
+pie = d3.pie()
+    .sort(null)
+    .value(d => d.value)
