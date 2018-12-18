2cd0a462de2aa091fde9a49236882140eee94433
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-18 19:11:53 +0200

    Update script_5.js

--- assets/scripts/script_5.js
+++ assets/scripts/script_5.js
@@ -1,134 +1,56 @@
-var data = [{
-  "country": "Belgium",
-  "EUR/month": 1,501.82,
-}, {
-  "country": "Bulgaria",
-  "EUR/month": 184.07,
-}, {
-  "country": "Czechia",
-  "EUR/month": 331.71,
-}, {
-  "country": "Germany",
-  "EUR/month":  1,440.00,
-}, {
-  "country": "Estonia",
-  "EUR/month": 390.00,
-}, {
-  "country": "Ireland",
-  "EUR/month": 1,461.85,
-}, {
-  "country": "Greece",
-  "EUR/month": 683.76,
-}, {
-  "country": "Spain",
-  "EUR/month": 756.70,
-}, {
-  "country": "France",
-  "EUR/month": 1,457.52,
-}, {
-  "country": "Croatia",
-  "EUR/month": 395.61,
-}];
+var data = [
+  {country: "Belgium", walking: 0, cycling: 0, aerobic: 0, strength: 0},
+  {country: "Bulgaria", walking: 91.4, cycling: 10.0, aerobic: 10.8, strength: 10.5},
+  {country: "Czechia", walking:  92.2, cycling:  35.5, aerobic: 34.7, strength: 19.3},
+  {country: "Denmark", walking:  79.1, cycling:  47.1, aerobic: 73.8, strength: 48.3}
+];
 
+var series = d3.stack()
+    .keys(["walking", "cycling", "aerobic", "strength"])
+    .offset(d3.stackOffsetDiverging)
+    (data);
 
-var margin = {
-    top: 40,
-    right: 40,
-    bottom: 100,
-    left: 60
-  },
-  width = 800 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom,
-  x = d3.scaleBand().rangeRound([15, width]).paddingInner(0.5),
-  y = d3.scaleLinear().range([height, 0]);
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 30, bottom: 30, left: 60},
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-//draw axis
-var xAxis = d3.axisBottom().scale(x).ticks(6);
+var x = d3.scaleBand()
+    .domain(data.map(function(d) { return d.country; }))
+    .rangeRound([margin.left, width - margin.right])
+    .padding(0.1);
 
-var yAxis = d3.axisLeft().scale(y).ticks(5).tickSizeInner(-width).tickSizeOuter(0).tickPadding(10);
+var y = d3.scaleLinear()
+    .domain([d3.min(series, stackMin), d3.max(series, stackMax)])
+    .rangeRound([height - margin.bottom, margin.top]);
 
-var svg = d3.select("#barGraph")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-x.domain(data.map(function(d) {
-  return d.year;
-}));
-
-y.domain([0, d3.max(data, function(d) {
-  return d.rank;
-})]);
+var z = d3.scaleOrdinal(d3.schemeCategory10);
 
 svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0, " + height + ")")
-  .call(xAxis)
-  .selectAll("text")
-  .style("text-anchor", "end")
-  .attr("dx", "-0.5em")
-  .attr("dy", "-.55em")
-  .attr("y", 30)
-  .attr("transform", "rotate(-45)");
+  .selectAll("g")
+  .data(series)
+  .enter().append("g")
+    .attr("fill", function(d) { return z(d.key); })
+  .selectAll("rect")
+  .data(function(d) { return d; })
+  .enter().append("rect")
+    .attr("width", x.bandwidth)
+    .attr("x", function(d) { return x(d.data.country); })
+    .attr("y", function(d) { return y(d[1]); })
+    .attr("height", function(d) { return y(d[0]) - y(d[1]); })
 
 svg.append("g")
-  .attr("class", "y axis")
-  .call(yAxis);
+    .attr("transform", "translate(0," + y(0) + ")")
+    .call(d3.axisBottom(x));
 
-svg.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 5)
-  .attr("dy", "0.8em")
-  .attr("text-anchor", "end");
+svg.append("g")
+    .attr("transform", "translate(" + margin.left + ",0)")
+    .call(d3.axisLeft(y));
 
-svg.selectAll("bar")
-  .data(data)
-  .enter()
-  .append("rect")
-  .style("fill", "orange")
-  .attr("x", function(d) {
-    return x(d.year);
-  })
-  .attr("width", x.bandwidth())
-  .attr("y", function(d) {
-    return y(d.rank);
-  })
-  .attr("height", function(d) {
-    return height - y(d.rank);
-  })
-  .on("mouseover", function() {
-    tooltip.style("display", null);
-  })
-  .on("mouseout", function() {
-    tooltip.style("display", "none");
-  })
+function stackMin(serie) {
+  return d3.min(serie, function(d) { return d[0]; });
+}
 
-.on("mousemove", function(d) {
-
-  tooltip.transition().duration(200)
-    .style("opacity", 0.9);
-  tooltip.select("div").html("year: <strong>" + d.year + "</strong><br/>Rank: <strong>" + d.rank + "</strong>")
-    .style("position", "fixed")
-    .style("left", (d3.event.pageX) + "px")
-    .style("top", (d3.event.pageY - 28) + "px");
-
-});
-
-var tooltip = d3.select("body").append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0.5);
-
-tooltip.append("rect")
-  .attr("width", 30)
-  .attr("height", 20)
-  .attr("fill", "#ffffff")
-  .style("opacity", 0.5);
-
-tooltip.append("div")
-  .attr("x", 15)
-  .attr("dy", "1.2em")
-  .style("text-anchor", "middle")
-  .attr("font-size", "1.5em")
-  .attr("font-weight", "bold");
+function stackMax(serie) {
+  return d3.max(serie, function(d) { return d[1]; });
+}
