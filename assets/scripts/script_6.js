aae656499247dca59bccfebe3b24a6259a89ce12
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-18 19:33:08 +0200

    Update script_6.js

--- assets/scripts/script_6.js
+++ assets/scripts/script_6.js
@@ -1,3 +1,60 @@
+var data = [
+  {country: "Germany", 2006: 78.4, 2015: 73.3},
+  {country: "France", 2006: 70.0, 2015: 77.7},
+  {country: "Spain", 2006:  65.6, 2015:  58.5},
+  {country: "Greece", 2006:  47.9, 2015:  46.9}
+];
+
+var series = d3.stack()
+    .keys(["2006", "2015"])
+    .offset(d3.stackOffsetDiverging)
+    (data);
+
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 30, bottom: 30, left: 60},
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var x = d3.scaleBand()
+    .domain(data.map(function(d) { return d.country; }))
+    .rangeRound([margin.left, width - margin.right])
+    .padding(0.1);
+
+var y = d3.scaleLinear()
+    .domain([d3.min(series, stackMin), d3.max(series, stackMax)])
+    .rangeRound([height - margin.bottom, margin.top]);
+
+var z = d3.scaleOrdinal(d3.schemeCategory10);
+
+svg.append("g")
+  .selectAll("g")
+  .data(series)
+  .enter().append("g")
+    .attr("fill", function(d) { return z(d.key); })
+  .selectAll("rect")
+  .data(function(d) { return d; })
+  .enter().append("rect")
+    .attr("width", x.bandwidth)
+    .attr("x", function(d) { return x(d.data.country); })
+    .attr("y", function(d) { return y(d[1]); })
+    .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+
+svg.append("g")
+    .attr("transform", "translate(0," + y(0) + ")")
+    .call(d3.axisBottom(x));
+
+svg.append("g")
+    .attr("transform", "translate(" + margin.left + ",0)")
+    .call(d3.axisLeft(y));
+
+function stackMin(serie) {
+  return d3.min(serie, function(d) { return d[0]; });
+}
+
+function stackMax(serie) {
+  return d3.max(serie, function(d) { return d[1]; });
+}
+
 var chart = c3.generate({
   bindto: d3.select('.chart'),
   data: {
