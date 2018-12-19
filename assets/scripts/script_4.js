93dca48b4010866ae873b718d42ab4c9ab032a27
Σωκράτης Καραουστάς <43907317+p17kara@users.noreply.github.com>
2018-12-19 16:14:41 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -1,93 +1,66 @@
-var data = [{
-  'name':"Belgium",'value': 11 
-},{
-  'name':"Bulgaria",'value': 7
-},{
-  'name':"Czechia",'value': 10
-},{
-  'name':"Denmark",'value': 5
-},{
-  'name':"Estonia", 'value': 1
-},{
-  'name':"Ireland",'value': 4
-},{
-  'name':"Greece",'value': 10
-}];
+//Donut chart example
+	nv.addGraph(function() {
+	  var chart = nv.models.pieChart()
+	      .x(function(d) { return d.label })
+	      .y(function(d) { return d.value })
+	      .showLabels(true)     //Display pie labels
+	      .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+	      .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+	      .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+	      .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+	      ;
+	
+	    d3.select("#chart2 svg")
+	        .datum(exampleData())
+	        .transition().duration(350)
+	        .call(chart);
+	
+	  return chart;
+	});
+	
+	//Pie chart example data. Note how there is only a single array of key-value pairs.
+	function exampleData() {
+	  return  [
+	      { 
+	        "label": "Belgium",
+	        "value" : 608182
+	      } ,
+	      { 
+	        "label": "Bulgaria",
+	        "value" : 213984
+	      } ,
+	      { 
+	        "label": "Czechia",
+	        "value" : 213984
+	      } ,
+	      { 
+	        "label": "Denmark",
+	        "value" : 173993
+	      } ,
+	      { 
+	        "label": "Germany",
+	        "value" : 325182
+	      } ,
+	      { 
+	        "label": "Estonia",
+	        "value" : 162591
+	      } ,
+	      { 
+	        "label": "Ireland",
+	        "value" : 162591
+	      } ,
+	      { 
+	        "label": "Greece",
+	        "value" : 144449
+	      } , 
+	      { 
+	        "label": "Spain",
+	        "value" : 112486
+	      } ,
+	      { 
+	        "label": "France",
+	        "value" : 112486
+	      }
+	    ];
+	}
 
-
-// Set the dimensions of our chart to be displayed 
-var barsWidth = 500,
-	barsHeight = 400,
-	axisMargin = 100;
-
-var chartHeight = barsHeight+axisMargin,
-	chartWidth = barsWidth+axisMargin;
-
-
-// Select the chart element on the page so we can reference it in code
-// Also set the width and height attributes of the chart SVG 
-var chart = d3.select('#chart')
-	.attr('width', chartWidth+100)
-	.attr('height', chartHeight);
-
-// Create a linear scale for our y-axis to map datapoint values to pixel heights of bars
-var yScale = d3.scaleLinear()
-	.domain([0,d3.max(data, function(d){
-    // return the value property of each datapoint so the max function can compare
-		return d.value;
-	})])
-	.rangeRound([barsHeight, 0]);
-
-// Create a scale that returns the bands each bar should be in along the x-axis
-let xScale = d3.scaleBand()
-	.domain(
-		data.map(
-			function(d){
-        // For each datapoint in our data array
-        // Return the name property into our new domain array
-				return d.name;
-			}
-		)
-	)
-	.rangeRound([0,barsWidth])
-	.padding(0.1);
-
-// Create an SVG group that we will add the individual bar elements of our chart to
-var bars = chart.append('g')
-	.attr('id', "bars-container");
-
-// Bind the data to our .bars svg elements
-// Create a rectangle for each data point and set position and dimensions using scales
-bars.selectAll('.bar')
-	.data(data)
-	.enter().append("rect")
-		.attr('class', "bar")
-		.attr('x', function(d){
-			return xScale(d.name);
-		})
-		.attr('y', function(d){
-			return yScale(d.value); 
-		})
-		.attr('width', xScale.bandwidth())
-		.attr('height', function(d){return barsHeight-yScale(d.value);});
-
-// Move the bars so that there is space on the left for the y-axis
-bars.attr('transform', 'translate('+axisMargin+',0)');
-
-// Create a new SVG group for the y-axis elements
-// Generate the y-axis with 10 ticks and move into position
-yAxis = chart.append('g')
-	.attr('id','y-axis')
-	.call(d3.axisLeft(yScale).ticks(10))
-		.attr('transform', 'translate('+axisMargin+',0)');
-
-// Create another group for the x-axis elements
-// Generate the x-axis using the our x scale and move into positon
-// Select the text elements and rotate by 45 degrees
-xAxis = chart.append('g')
-	.attr('id', 'x-axis')
-	.call(d3.axisBottom(xScale))
-	.attr('transform', 'translate('+axisMargin+','+barsHeight+')')
-	.selectAll("text")
-		.style("text-anchor",'start')
-		.attr('transform', 'rotate(45)');
